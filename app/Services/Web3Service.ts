d431d731f028751a0353f27f446bbe5133d6385f
Augusto Vicente <devguto@gmail.com>
2022-12-01 23:11:27 -0300

    some fixes for backend

--- app/Services/Web3Service.ts
+++ app/Services/Web3Service.ts
@@ -4,17 +4,16 @@ import Web3 from 'web3';
 import Env from '@ioc:Adonis/Core/Env';
 import { abi, contract_address } from 'App/Solidity/contract';
 
-const provider = new HDWalletProvider(
-    Env.get('WALLET_WORDS'),
-    "https://goerli.infura.io/v3/fee8917ab09e4e409ada6f602b288672"
-);
+// const endpoint = "https://polygon-rpc.com/";
+const endpoint = "https://goerli.infura.io/v3/fee8917ab09e4e409ada6f602b288672";
+const provider = new HDWalletProvider( Env.get('WALLET_WORDS'), endpoint );
 const nonceTracker = new NonceTrackerSubprovider()
 provider.engine._providers.unshift(nonceTracker)
 nonceTracker.setEngine(provider.engine)
 
 const web3 = new Web3(provider);
 const contract = new web3.eth.Contract(abi as any, contract_address);
-const no_wallet_web3 = new Web3("https://goerli.infura.io/v3/fee8917ab09e4e409ada6f602b288672");
+const no_wallet_web3 = new Web3(endpoint);
 
 const mint_package = async (pack_type: 1|2|3, address: string, stickers:number[], amount:number) =>
 {
@@ -23,6 +22,11 @@ const mint_package = async (pack_type: 1|2|3, address: string, stickers:number[]
     // mintando nfts
     return await contract.methods.mint_stycker_pack(stickers, address, pack_type, amount)
         .send({ from: contract_account })
+        // from: contract_account,
+        // maxPriorityFeePerGas: web3.utils.toWei('30', 'Gwei'),
+        // gasPrice: web3.utils.toWei('35', 'Gwei'),
+        // gasLimit: 177365,
+        // maxFeePerGas: web3.utils.toWei('35', 'Gwei'),
 }
 
 const burn_for_mint = async (stickers_to_burn: number[], address: string, stickers_to_mint:number[]) =>
